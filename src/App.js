1762979b9d328660b5fbf5ffde79c071c4182c2c
Aelish Boghani <boghaniaelish@gmail.com>
2022-01-13 12:48:48 +0530

    add new componenets

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import { useState } from 'react';
 import './App.css';
+import EmiOptionsCard from './components/EmiOptionsCard';
 import LenderCard from './components/LenderCard';
+import Modal from './components/Modal';
+
 
 
 function App() {
+  const [modalOpen, setModalOpen] = useState(false);
   return (
     <div className="App">
-      <LenderCard/>
+      {/* <LenderCard/> */}
+      <EmiOptionsCard/>
+      
+      {/* <button
+        className="openModalBtn"
+        onClick={() => {
+          setModalOpen(true);
+        }}
+      >
+        Open
+      </button>
+
+      {modalOpen && <Modal setOpenModal={setModalOpen} />} */}
+     
     </div>
   );
 }
